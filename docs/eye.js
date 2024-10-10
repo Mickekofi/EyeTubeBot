f07d7bbcb9443c736e09725941e625a12e4b54ee
Michael Appiah <mickekofi6@gmail.com>
2024-10-10 06:40:35 +0000

    update

---
+++ docs/eye.js
@@ -0,0 +1,12 @@
+// Custom JavaScript functionality can be added here
+
+// Example: Smooth scroll to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
